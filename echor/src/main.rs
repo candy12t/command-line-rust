ca83359373aab48553ad9785b7caa8f67ee8cff3
Takumi Kanada <candy12t@gmail.com>
2024-01-21 00:17:36 +0900

    Add: clap, parse command line args library(#2)

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,9 @@
+use clap::App;
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let _matches = App::new("echor")
+        .version("0.1.0")
+        .author("candy12t")
+        .about("Rust echo")
+        .get_matches();
 }
